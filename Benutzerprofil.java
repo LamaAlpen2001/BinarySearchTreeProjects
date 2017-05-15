5455199abf747cc64854ce96322af8e0af766e47
PiJay <pj.seidenstuecker@gmx.de>
2017-05-15 15:27:23 +0200

    Initial sharing of project

---
+++ Benutzerprofil.java
@@ -0,0 +1,5 @@
+public class Benutzerprofil implements ComparableContent<Benutzerprofil>{
+
+    //EIGENER CODE
+
+}
---
+++ Benutzerverwaltung.java
@@ -0,0 +1,5 @@
+public class Benutzerverwaltung{
+    
+    //EIGENER CODE
+    
+}
