05afc91e3f372e0e5c75b6e445690e2958f5be07
ErikNathrath <erik@nathrath.com>
2017-05-15 15:28:32 +0200

    the revolution in Benutzer Management

--- Benutzerprofil.java
+++ Benutzerprofil.java
@@ -1,5 +1,25 @@
 public class Benutzerprofil implements ComparableContent<Benutzerprofil>{
+    private String benutzername;
+    private String passwort;
+    
+    public Benutzerprofil(String login,String pw){
+        System.out.println("Wilkommen bei E&P.inc Benutzerspeicherung. Sie wurden Gespeichert");
+        passwort= pw;
+        benutzername= login;
 
-    //EIGENER CODE
-
+    }
+    public boolean isLess(Benutzerprofil pBenutzer){
+        return (this.benutzername.compareTo(pBenutzer.getBenutzername())<0);
+        
+    }
+    public boolean isGreater(Benutzerprofil pBenutzer){
+        return (this.benutzername.compareTo(pBenutzer.getBenutzername())>0);
+    }
+    public boolean isEqual(Benutzerprofil pBenutzer){
+        return (this.benutzername.compareTo(pBenutzer.getBenutzername())==0);
+    }
+    public String getBenutzername(){
+        return benutzername;
+    }
 }
+
